bd2121acf76fcbb446ee377cf40e7d821c75ac43
Egor Chernakov <eg.cher.2707@gmail.com>
2023-05-03 16:51:43 +0300

    Add aiosqlite integration

--- db_broker.py
+++ db_broker.py
@@ -1,25 +1,50 @@
 import sqlite3
-from time import sleep
+import aiosqlite
 import asyncio
 
-reports = [
+def get_select_queries(id: str):
+    report_query = 'SELECT 1;'
+    pdf_query = 'SELECT 1;'
+    section_query = 'SELECT 1;'
+    hyperlink_query = 'SELECT 1;'
+    return [report_query, pdf_query, section_query, hyperlink_query]
 
-    {'id': 'abc', 'name': 'Отчёт за январь', 'pdf_num': 1},
-    {'id': 'def', 'name': 'Отчёт за февраль', 'pdf_num': 1},
-    {'id': 'ghi', 'name': 'Отчёт за март', 'pdf_num': 1},
-    {'id': 'jkl', 'name': 'Отчёт за апрель', 'pdf_num': 1},
-]
+def sleep_query(num):
+    query = f"""
+    WITH RECURSIVE r(i) AS (
+  VALUES(0)
+  UNION ALL
+  SELECT i FROM r
+  LIMIT {num}0000000
+  )
+  SELECT i FROM r WHERE i = 1;
+    """
+    return query
+
+async def fetch_async(query, db1):
+    async with aiosqlite.connect(database) as db:
+        db.row_factory = row_factory
+        async with db.execute(query) as cursor:
+            res = await cursor.fetchall()
+            return res
 
 def fetch(query):
-    res = cur.execute(query)
-    return res.fetchall()
+    con = sqlite3.connect(database)
+    con.row_factory = row_factory
+    res = con.execute(query).fetchall()
+    con.close()
+    return res
 
-def get_report(id: str):
-    pass
+async def get_report(id: str):
+
+        tasks = [asyncio.create_task(fetch_async(query, 1)) for query in get_select_queries(id)]
+        done, pending = await asyncio.wait(tasks)
+        return [task.result() for task in done]
 
 def get_reports():
-    res = cur.execute("SELECT * FROM report")
-    return res.fetchall()
+    # res = con.execute("SELECT * FROM report").fetchall()
+    # res = cur.execute("SELECT * FROM report")
+    return fetch("SELECT * FROM report")
 
 def update_pdf(id: str, pdf_num: int, pdf: bytes):
     pass
@@ -33,23 +58,16 @@ def delete_report(id: str):
 def insert_report(id: str):
     pass
 
-async def fetchall_async(conn, query):
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(
-        None, lambda: conn.cursor().execute(query).fetchall())
-
 def row_factory(cursor: sqlite3.Cursor, row):
     keys = [result[0] for result in cursor.description]
     return {key: value for key, value in zip(keys, row)}
 
 database = 'dev.db'
 
-con = sqlite3.connect(database)
-con.row_factory = row_factory
-cur = con.cursor()
+# cur = con.cursor()
 
-def main():   
-    print(fetch("SELECT * from report where id_report = '1'"))
+async def main():   
+    print(await get_report('123'))
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
