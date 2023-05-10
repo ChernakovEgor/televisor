85669b749a8c6c5b46b66354f0a2cf50147904fc
Egor Chernakov <eg.cher.2707@gmail.com>
2023-05-10 18:34:42 +0300

    Add row identification to update_report

--- assets_manager.py
+++ assets_manager.py
@@ -1,6 +1,5 @@
 import os
 import logging
-from io import BytesIO
 import filecmp
 import time
 
@@ -11,16 +10,16 @@ icon_dir = 'data/icons/'
 def generate_name(report_id: str, num: int):
     local_time = time.localtime()
     time_string = time.strftime("%S%M%H%d%m%Y", local_time)
-    seconds = time.time()
+    # seconds = time.time()
     return f"{report_id}_{num}_{time_string}.pdf"
 
 def save_pdf(name: str, data: bytes):
-    pdfs = os.listdir("data/pdfs/")
-    path_to_write = f"data/pdfs/{name}"
+    pdfs = os.listdir(pdf_dir)
+    path_to_write = pdf_dir + name
     with open(path_to_write, 'wb') as f:
         f.write(data) 
     for pdf in pdfs:
-        comparison = filecmp.cmp(path_to_write, f"data/pdfs/{pdf}",  shallow=False)
+        comparison = filecmp.cmp(path_to_write, pdf_dir + pdf,  shallow=False)
         if comparison:
             os.remove(path_to_write)
             return pdf
@@ -35,29 +34,35 @@ def clean_up(pdfs_in_table):
     
 
 def delete_pdf(name):
-    if os.path.exists(f"data/pdfs/{name}"):
-        os.remove(f"data/pdfs/{name}")
+    if os.path.exists(pdf_dir + name):
+        os.remove(pdf_dir + name)
 
 def get_pdf(name: str):
-    with open(f"data/pdfs/{name}", 'rb') as f:
+    with open(pdf_dir + name, 'rb') as f:
        return f.read()
 
 def save_icon(name: str, data: bytes):
-    path = f"data/icons/{name}"
-    with open(path, 'wb') as f:
-        f.write(data)
-        return path
+    icons = os.listdir(icon_dir)
+    path_to_write = icon_dir + name
+    with open(path_to_write, 'wb') as f:
+        f.write(data) 
+    for icon in icons:
+        comparison = filecmp.cmp(path_to_write, icon_dir + icon, shallow=False)
+        if comparison:
+            os.remove(path_to_write)
+            return icon
+    return name
 
 def get_icon(name: str):
-    with open(f"data/icons/{name}", 'rb') as f:
+    with open(icon_dir + name, 'rb') as f:
        return f.read()
 
 def delete_icon(name):
-    if os.path.exists(f"data/icons/{name}"):
-        os.remove(f"data/icons/{name}")
+    if os.path.exists(icon_dir + name):
+        os.remove(icon_dir + name)
 
 def main():
-    clean_up(['dummy.pdf'])
+    save_pdf('testing', b'999999999')
 
 if __name__ == "__main__":
     main()
